2b27bcdb1da363263a92e6f46af6fd766a65314f
WendyWAAAAANG <91651767+WendyWAAAAANG@users.noreply.github.com>
2023-12-20 14:05:01 +0800

    Add files via upload

---
+++ dpwII_project/dpwII_project/urls.py
@@ -0,0 +1,25 @@
+"""karlie URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    # add urls of minisearch as main page.
+    path('', include('minisearch.urls')),
+    # add urls of tfidf-index as result page.
+    #path('tfidf/', include('tfidf-index.urls')),
+    path('admin/',admin.site.urls),
+]
